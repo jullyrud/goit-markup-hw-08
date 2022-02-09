45d7ae2b015245c01483f7c8708d84474581f335
jullyrud <jullyrud@gmail.com>
2022-02-10 00:46:45 +0200

    hw9

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,33 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-Menu-open]'),
     closeMenuBtn: document.querySelector('[data-Menu-close]'),
-    Menu: document.querySelector('[data-Menu]'),
-  };
+      Menu: document.querySelector('[data-Menu]'),
+      body: document.querySelector('body'),
+    };
+    // console.log(refs.body)
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.Menu.classList.toggle('is-hidden');
-  }
+    refs.body.classList.toggle('no-scroll');
+
+    }
+    window.addEventListener('resize', () => {
+        if (window.innerWidth >= 768) {
+        !refs.Menu.classList.contains('is-hidden') ?
+            refs.Menu.classList.add('is-hidden') : '';
+        
+        if (refs.modal) {
+            refs.body.classList.contains('no-scroll')
+                && refs.modal.classList.contains('is-hidden')
+                ? refs.body.classList.remove('no-scroll') : '';
+        } else {
+            refs.body.classList.contains('no-scroll')
+                ? refs.body.classList.remove('no-scroll') : '';
+        }
+
+        }
+    })
 })();
